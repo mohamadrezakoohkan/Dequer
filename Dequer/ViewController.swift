b7c71c35794ce633cabcb4149a08df7c77a37220
mohamad reza koohkan <mohamad_koohkan@icloud.com>
2019-08-16 21:25:51 +0430

    Initial Commit

---
+++ Dequer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dequer
+//
+//  Created by Mohammad reza Koohkan on 5/25/1398 AP.
+//  Copyright © 1398 AP Apple Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
